adb11ed96af9d71f1385d9758d0ece417d9bbc86
Polina <pelepolya@yandex.ru>
2023-06-05 02:54:07 +0300

    fixed ProjectTabLayoutFragment.kt

--- app/src/main/java/com/pelepolya/designerplanner/ProjectTabLayoutFragment.kt
+++ app/src/main/java/com/pelepolya/designerplanner/ProjectTabLayoutFragment.kt
@@ -5,39 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-import com.pelepolya.designerplanner.databinding.FragmentProjectTabLayoutBinding
 
 class ProjectTabLayoutFragment : Fragment() {
-    private var binding: FragmentProjectTabLayoutBinding? = null
     var projectPagerAdapter: ProjectPagerAdapter? = null
+
+    private lateinit var viewPagerProject: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentProjectTabLayoutBinding.inflate(inflater, container, false)
-        return binding!!.root
+    ): View {
+        return inflater.inflate(R.layout.fragment_project_tab_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewPagerProject = view.findViewById(R.id.view_pager_project)
+        tabLayout = view.findViewById(R.id.tab_layout)
+
         projectPagerAdapter = ProjectPagerAdapter(requireActivity())
-        binding!!.viewPagerProject.adapter = projectPagerAdapter
-        binding!!.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+        viewPagerProject.adapter = projectPagerAdapter
+        tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                binding!!.viewPagerProject.currentItem = tab.position
+                viewPagerProject.currentItem = tab.position
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
-        binding!!.viewPagerProject.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+        viewPagerProject.registerOnPageChangeCallback(object : OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                binding!!.tabLayout.getTabAt(position)!!.select()
+                tabLayout.getTabAt(position)!!.select()
             }
         })
     }
