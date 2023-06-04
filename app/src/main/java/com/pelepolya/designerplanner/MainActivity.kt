51dacd21f7ea2ae5869fa2f83f090effe88b34c8
Polina <pelepolya@yandex.ru>
2023-06-04 14:40:08 +0300

    Initial commit

---
+++ app/src/main/java/com/pelepolya/designerplanner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pelepolya.designerplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
