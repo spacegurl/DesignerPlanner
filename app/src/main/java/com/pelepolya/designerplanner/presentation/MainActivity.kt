a4c773277265f911f911d13d76b65d9b6bf6ae15
Polina <pelepolya@yandex.ru>
2023-06-04 22:47:05 +0300

    added user authorization

--- app/src/main/java/com/pelepolya/designerplanner/presentation/MainActivity.kt
+++ app/src/main/java/com/pelepolya/designerplanner/presentation/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.pelepolya.designerplanner.presentation
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.pelepolya.designerplanner.R
+import com.pelepolya.designerplanner.presentation.fragments.auth.SignInFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check session
+        val sharedPrefRead: SharedPreferences = getPreferences(MODE_PRIVATE)
+        val logined = sharedPrefRead.getString(SignInFragment.AUTH_SESSION, "")
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragments_container) as NavHostFragment
+        val graph = navHost.navController
+            .navInflater.inflate(R.navigation.main_graph)
+        if (logined != "") {
+            graph.setStartDestination(R.id.menuFragment)
+        } else {
+            graph.setStartDestination(R.id.startFrameFragment2)
+        }
+        navHost.navController.graph = graph
+
     }
 }
--- app/src/main/java/com/pelepolya/designerplanner/presentation/fragments/auth/SignInFragment.kt
+++ app/src/main/java/com/pelepolya/designerplanner/presentation/fragments/auth/SignInFragment.kt
@@ -1,16 +1,25 @@
 package com.pelepolya.designerplanner.presentation.fragments.auth
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import com.pelepolya.designerplanner.R
 import com.pelepolya.designerplanner.databinding.FragmentSignInBinding
+import com.pelepolya.designerplanner.domain.entity.SignInUser
+import com.pelepolya.designerplanner.presentation.viewmodel.auth.SignInViewModel
 
-class SignInFragment: Fragment() {
+class SignInFragment : Fragment() {
 
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: SignInViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,9 +30,48 @@ class SignInFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            buttonSignin.setOnClickListener {
+                viewModel.validAuth(
+                    SignInUser(
+                        phone = enterPhone.text.toString(),
+                        password = enterPasswd.text.toString()
+                    )
+                )
+                viewModel.checkAuthValid.observe(viewLifecycleOwner) {
+                    if (it) {
+                        val sharedPrefWrite = requireActivity().getPreferences(Context.MODE_PRIVATE)
+                        val editor = sharedPrefWrite.edit()
+                        editor.putString(
+                            AUTH_SESSION,
+                            enterPhone.text.toString()
+                        )
+                        editor.apply()
+
+                        val action = SignInFragmentDirections.actionSignInFragment2ToMenuFragment()
+                        view.findNavController().navigate(action)
+                    } else {
+                        textInputPhone.helperText = getString(R.string.invalid_error)
+                        enterPhone.doOnTextChanged { _, _, _, _ ->
+                            textInputPhone.helperText = ""
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+
+        const val AUTH_SESSION = "auth"
     }
 
 }
