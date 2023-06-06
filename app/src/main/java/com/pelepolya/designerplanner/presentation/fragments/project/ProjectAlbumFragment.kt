9518d20dc2d4a49d08ea19a0fddcd6933268c3c6
Polina <>
2023-06-07 01:21:37 +0300

    added gallery

--- app/src/main/java/com/pelepolya/designerplanner/presentation/fragments/project/ProjectAlbumFragment.kt
+++ app/src/main/java/com/pelepolya/designerplanner/presentation/fragments/project/ProjectAlbumFragment.kt
@@ -1,17 +1,22 @@
 package com.pelepolya.designerplanner.presentation.fragments.project
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.pelepolya.designerplanner.R
 import com.pelepolya.designerplanner.databinding.FragmentProjectAlbumBinding
+
 
 class ProjectAlbumFragment : Fragment() {
 
     private var _binding: FragmentProjectAlbumBinding? = null
     private val binding get() = _binding!!
+
+    private val GALLERY_REQ_CODE = 1000
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +29,23 @@ class ProjectAlbumFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.btnGallery.setOnClickListener(View.OnClickListener {
+            val iGallery = Intent(Intent.ACTION_PICK)
+            iGallery.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            startActivityForResult(iGallery, GALLERY_REQ_CODE)
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == GALLERY_REQ_CODE) {
+                // for gallery
+
+                binding.imgGallery.setImageURI(data?.data);
+            }
+        }
     }
 
     override fun onDestroyView() {
