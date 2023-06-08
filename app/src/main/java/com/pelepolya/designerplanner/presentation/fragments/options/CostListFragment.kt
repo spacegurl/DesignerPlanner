3d695ae98517a9564c931e2e3d8d72b62bf90229
he <>
2023-06-08 03:21:52 +0300

    feat: added logout

--- app/src/main/java/com/pelepolya/designerplanner/presentation/fragments/options/CostListFragment.kt
+++ app/src/main/java/com/pelepolya/designerplanner/presentation/fragments/options/CostListFragment.kt
@@ -1,5 +1,7 @@
 package com.pelepolya.designerplanner.presentation.fragments.options
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +9,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.pelepolya.designerplanner.R
+import com.pelepolya.designerplanner.data.db.models.UserRoles
 import com.pelepolya.designerplanner.databinding.FragmentCostListBinding
+import com.pelepolya.designerplanner.presentation.MainActivity
+import com.pelepolya.designerplanner.presentation.fragments.auth.SignInFragment
 
 class CostListFragment : Fragment() {
 
@@ -26,10 +31,36 @@ class CostListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.account.setOnClickListener {
+            logOut()
+        }
+        binding.logout.setOnClickListener {
+            logOut()
+        }
+
         binding.patternButton.setOnClickListener {
 //            val action = CostListFragmentDirections.actionCostListFragmentToCostListTabLayoutFragment()
             findNavController().navigate(R.id.action_costListFragment_to_costListTabLayoutFragment2)
         }
+    }
+
+    private fun logOut() {
+        val sharedPrefWrite = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPrefWrite.edit()
+        editor.putString(
+            SignInFragment.AUTH_SESSION,
+            ""
+        )
+        editor.putString(
+            SignInFragment.AUTH_ROLE,
+            UserRoles.NONE.name
+        )
+        editor.apply()
+
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.flags = intent.flags or Intent.FLAG_ACTIVITY_NO_HISTORY
+        startActivity(intent)
+        requireActivity().finish()
     }
 
     override fun onDestroyView() {
